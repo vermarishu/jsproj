446c42a85110ed01ccf829c6a5cb499f00aaaa7a
vermarishu <vermarishu251@gmail.com>
2024-06-27 18:37:51 +0530

    about this

---
+++ constructor/app.js
@@ -0,0 +1,26 @@
+function User(username, age, gender) {
+    this.username = username;
+    this.age = age; 
+    this.gender = gender;
+
+    this.greetin = function () {
+        // console.log(`welcome ${this.username}`);
+    }
+    return this
+}
+
+const user1 = new User("rishu", 11, "male");
+const user2 = new User("girl", 10, "female");
+// console.log(user1);
+// console.log(user2 instanceof User);
+ 
+// user1.greetin();
+
+function multiple(num) {
+    return num*5
+}
+multiple.power = 2
+
+console.log(multiple(5));
+console.log(multiple.power);
+    console.log(multiple.prototype);
