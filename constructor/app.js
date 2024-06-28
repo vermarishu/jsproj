96f4416b99eb3f2d08e10e593ca931cde60c0b01
vermarishu <vermarishu251@gmail.com>
2024-06-28 18:44:57 +0530

    adding prototype inheritance

--- constructor/app.js
+++ constructor/app.js
@@ -26,3 +26,43 @@ console.log(multiple.power);
 //(. dot ) used in Object but we can also use in function
 
     console.log(multiple.prototype);
+
+
+    
+/// prototype 
+    let myName = ["rishu", "verma"];
+     
+    let myFullName = {
+        fullName: "rishu",
+        nickName: "verma",
+
+        thereName: function() {
+            console.log(`name is ${this.fullName}`);
+        } 
+    };
+
+  
+    // Object.prototype.rishuverma = function() {
+    //     console.log("my name is here");
+    // }
+    
+    Array.prototype.rishuverma = function() {
+        console.log("name is aviable in Object");
+    }
+
+    myName.rishuverma();
+    // myFullName.rishuverma();
+
+/// print acutual(trueLength) length of string
+
+    let anoterUsername = "rishuverma   ";
+
+    String.prototype.trueLnegth = function() {
+        console.log(`${this}`);
+        console.log(`${this.name}`);
+        console.log(`True length is: ${this.trim().length}`);
+    }
+
+    anoterUsername.trueLnegth()
+    "rishu   ".trueLnegth()
+    "verma".trueLnegth()
